afae2caa6f9fa172e37381b13b73f5b85f250785
yassineka1 <yassine.kabbaj91@gmail.com>
2023-03-29 09:02:35 -0700

    Update

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,28 +1,32 @@
 #include "main.h"
-
 /**
- * leet - function that encodes a string into 1337
- * @s: string array
- * Return: string array
+ *leet - a function that encodes a string into 1337.
+ *@s: string to be crypted .
+ *Return: pointer to s .
+ *Update: In this new code , i changed 2 arrays with 2 pointers, that
+ *are pointed to the BASS adresses of the 2 arrays.
+ *in addition to that , i assigned a pointer p to s.
  */
 
 char *leet(char *s)
 {
+	char *m = "aeotl";
+	char *M = "AEOTL";
+	int x[] = {'4', '3', '0', '7', '1'};
 	int i;
+	char *p = s;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s != '\0')
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
-	}
 
-	return (s);
+	for (i = 0; i < 5; i++)
+	{
+		if (*s == *(m + i) || *s == *(M + i))
+
+			*s = x[i];
+	}
+		s++;
+
+	}
+		return (p);
 }
