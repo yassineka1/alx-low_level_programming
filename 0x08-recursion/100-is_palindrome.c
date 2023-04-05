2f0516250fac4bcd716e2d70b3cb7d91b56757a9
yassineka1 <yassine.kabbaj91@gmail.com>
2023-04-05 13:10:35 -0700

    Function that returns 1 if a string is a palindrome and 0 if not

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,51 +1,40 @@
 #include "main.h"
-
 /**
- * check_strlen - Length of a string
- * @s: string
- * Return: Length of string
+ * _strlen_rec - Prints the length of a string.
+ * @s: the string to be printed
+ * Return: the length of string
  */
-
-int check_strlen(char *s)
+int _strlen_rec(char *s)
 {
-	int length = 0;
-
-	if (*(s + length))
-	{
-		length++;
-		length += check_strlen(s + length);
-	}
-	return (length);
-}
-
-/**
- * check_palind - Checks if a string is a palindrome
- * @s: string
- * @length: Length of string
- * @i: index of string
- * Return: -1 if string isa palindrome or 0 if not
- */
-
-int check_palind(char *s, int length, int i)
-{
-	if (s[i] == s[length / 2])
-		return (1);
-	if (s[i] == s[length - i - 1])
-		return (check_palind(s, length, i + 1));
+	if (s[0] != '\0')
+		return (1 + _strlen_rec(s + 1));
 	return (0);
 }
-
 /**
- * is_palindrome - Cheks if a string is a palindrome
- * @s: string
- * Return: -1 if the string is a palindrome or 0 if not
+ * palind_check - check if s is palindrome.
+ * @s: string base address.
+ * @i: left index.
+ * @k: rigth index.
+ * Return: 1 if s is palindrome, 0 otherwise.
  */
-
+int palind_check(char *s, int i, int k)
+{
+	if (s[i] == s[k])
+		if (i > k / 2)
+			return (1);
+		else
+			return (palind_check(s, i + 1, k - 1));
+	else
+		return (0);
+}
+/**
+ * is_palindrome - check if s is palindrome
+ * @s: base address for string.
+ *
+ * Return: 1 if n is prime, 0 otherwise.
+ */
 int is_palindrome(char *s)
 {
-	int i = 0;
-	int length = check_strlen(s)
-		if (!(*s))
-			return (1);
-	return (check_palind(s, length, i));
+	return (palind_check(s, 0, _strlen_rec(s) - 1));
 }
+
