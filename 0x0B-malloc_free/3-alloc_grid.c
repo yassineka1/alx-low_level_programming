1c0b764bdb8d788fc05c79b57eb46f9506755b3a
yassineka1 <yassine.kabbaj91@gmail.com>
2023-04-11 06:29:06 -0700

    Update

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,27 +11,27 @@
 
 int **alloc_grid(int width, int height)
 {
-	int * *2dim_arr;
+	int **dim_arr;
 	int i1, i2;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	2dim_arr = malloc(sizeof(int *) * height);
+	dim_arr = malloc(sizeof(int *) * height);
 
-	if (2dim_arr == NULL)
+	if (dim_arr == NULL)
 		return (NULL);
 
 	for (i1 = 0; i1 < height; i1++)
 	{
-		2dim_arr[i1] = malloc(sizeof(int) * width);
+	dim_arr[i1] = malloc(sizeof(int) * width);
 
-		if (2dim_arr[i1] == NULL)
+		if (dim_arr[i1] == NULL)
 		{
 			for (; i1 >= 0; i1--)
-				free(2dim_arr[i1]);
+				free(dim_arr[i1]);
 
-			free(2dim_arr);
+			free(dim_arr);
 			return (NULL);
 		}
 	}
@@ -39,8 +39,8 @@ int **alloc_grid(int width, int height)
 	for (i1 = 0; i1 < height; i1++)
 	{
 		for (i2 = 0; i2 < width; i2++)
-			2dim_arr[i1][i2] = 0;
+			dim_arr[i1][i2] = 0;
 	}
 
-	return (2dim_arr);
+	return (dim_arr);
 }
