42c95d58a3f07ae19b9a41096d4a5bfd3b6a075b
yassineka1 <yassine.kabbaj91@gmail.com>
2023-04-17 07:36:28 -0700

    Function that frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Function that frees gods
+ * @d: Dog
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
